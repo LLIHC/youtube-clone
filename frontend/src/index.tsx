365ea67b525518ff98f0e83888c9f2342a7c4878
kynk94 <kynk94@naver.com>
2022-01-15 17:47:29 +0900

    add: main Header component

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { RecoilRoot } from 'recoil';
 import './index.css';
+import React from 'react';
+
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+
 import App from './App';
 
 ReactDOM.render(
