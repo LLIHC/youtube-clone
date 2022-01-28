cab2a0e65b2579e4bcf1b590efbd128f445e9b95
LEEMINJOO <eminj1997@gmail.com>
2022-01-28 16:15:57 +0900

    YC-18 로그인 상태 정보 저장

--- frontend/src/components/google-login.tsx
+++ frontend/src/components/google-login.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 
 import Button from '@mui/material/Button';
 import { useGoogleLogin } from 'react-google-login';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+
+import { loggedInState } from '../state/login';
 
 const clientId = '986704124267-8p8q17fs47f0htp7s686suvu21rmdmf2.apps.googleusercontent.com';
 
 
 function GoogleLoginBtn() {
+  const setLoggedIn = useSetRecoilState(loggedInState);
 
   const onSuccess = async (response: any) => {
     const { googleId, profileObj: { email, name } } = response;
+
+    // TODO: 상태정보 저장
     console.log(
       { googleId, profileObj: { email, name } },
     );
+
+    setLoggedIn(true);
   };
 
   const onFailure = (error: any) => {
@@ -39,6 +47,8 @@ function GoogleLoginBtn() {
 
 
 function GoogleLoginComponent() {
+  const loggedIn = useRecoilValue(loggedInState);
+
   return (
     <GoogleLoginBtn />
   );
---
+++ frontend/src/state/login.ts
@@ -0,0 +1,7 @@
+import { atom } from 'recoil';
+
+
+export const loggedInState = atom({
+  key: 'loggedInState',
+  default: false,
+});
