fe5d479bf14cf608eb1070ceaf07bc357b429afb
chad <ec_park@korea.ac.kr>
2022-01-30 14:01:36 +0900

    add: rough Drawer

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styled from '@emotion/styled';
+import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
 import SearchIcon from '@mui/icons-material/Search';
-import InputBase from '@mui/material/InputBase';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import MuiDrawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
+import InputBase from '@mui/material/InputBase';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
+const drawerWidth = 240;
 
 const StyledAppBar = styled(AppBar)({
+  position: 'fixed',
   backgroundColor: '#fafafa',
   color: '#212121',
   display: 'block',
   height: '56px',
   width: '100%',
+  zIndex: '1400',
 });
 
 const StyledToolbar = styled(Toolbar)({
@@ -40,12 +52,37 @@ const SearchInputBase = styled(InputBase)({
   zIndex: 2,
 });
 
+interface StyledDrawerProps {
+  drawerOpened : boolean,
+}
 
+const StyledDrawer = styled(MuiDrawer)({
+  flexShrink: 0,
+  whiteSpace: 'nowrap',
+  boxSizing: 'border-box',
+  anchor: 'left',
+}, ({ drawerOpened }: StyledDrawerProps)=> ({
+  ...(drawerOpened && {
+    width: 60,
+    '& .MuiDrawer-paper': { width: 60 },
+  }),
+  ...(!drawerOpened && {
+    width: 120,
+    '& .MuiDrawer-paper': 120,
+  }),
+}));
 export default function Header() {
+  const [opened, setOpen] = useState(true);
+
+  const handleDrawer = () => {
+    setOpen(!opened);
+  };
+
   return (
+    <Box>
     <StyledAppBar>
       <StyledToolbar variant="dense">
-        <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+        <IconButton onClick={handleDrawer} size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
           <MenuIcon />
         </IconButton>
         <Box sx={{ flexGrow: 1 }}>
@@ -62,5 +99,30 @@ export default function Header() {
         </Button>
       </StyledToolbar>
     </StyledAppBar>
+    <StyledDrawer variant="permanent" drawerOpened={opened}>
+    <Toolbar />
+    <List>
+      {['홈', '탐색', '구독', 'originals'].map((text, index) => (
+        <ListItem button key={text}>
+          <ListItemIcon>
+            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+          </ListItemIcon>
+          <ListItemText primary={text} />
+        </ListItem>
+      ))}
+    </List>
+    <Divider />
+    <List>
+      {['홈', '탐색', '구독', 'originals'].map((text, index) => (
+        <ListItem button key={text}>
+          <ListItemIcon>
+            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+          </ListItemIcon>
+          <ListItemText primary={text} />
+        </ListItem>
+      ))}
+    </List>
+  </StyledDrawer>
+  </Box>
   );
 }
