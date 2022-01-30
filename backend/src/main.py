d1542dba9f07c29c5e20494a2456a5000760ec40
hunhoon21 <hunhoon21@gmail.com>
2022-01-30 16:24:32 +0900

    :hammer: simple read contents

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,7 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from uvicorn.config import LOGGING_CONFIG
+from sqlalchemy.orm import Session
 
 from app.api import api_router
 
@@ -8,6 +9,16 @@ app = FastAPI(
     title="Backend", version="1.0", description="Serving Youtube Backend"
 )
 app.include_router(api_router)
+
+# Dependency
+def get_db(request: Request):
+    return request.state.db
+
+@app.get("/contents/", response_model)
+def read_contents(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    contents = crud.get_contents(db, skip=skip, limit=limit)
+    return contents
+
 
 if __name__ == "__main__":
     LOGGING_CONFIG["formatters"]["default"][
