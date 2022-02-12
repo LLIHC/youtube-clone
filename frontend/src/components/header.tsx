ed90cb5feb620c30ca52b91a1c60a48ee300fb78
chad <ec_park@korea.ac.kr>
2022-02-12 12:44:07 +0900

    fix: extract CustomDrawer from header to drawer

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,27 +1,18 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 
 import styled from '@emotion/styled';
-import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import SearchIcon from '@mui/icons-material/Search';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import MuiDrawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import InputBase from '@mui/material/InputBase';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import Toolbar from '@mui/material/Toolbar';
 
 import AccountButton from './account/account';
 import LogoButton from './logo-button';
-const drawerWidth = 240;
+import CustomDrawer from './drawer';
 
 const StyledAppBar = styled(AppBar)({
   position: 'fixed',
@@ -64,32 +55,19 @@ const AlignBox = styled(Box)({
   display: 'flex',
   alignItems: 'center',
 });
-interface StyledDrawerProps {
-  drawerOpened : boolean,
-}
 
-const StyledDrawer = styled(MuiDrawer)({
-  flexShrink: 0,
-  whiteSpace: 'nowrap',
-  boxSizing: 'border-box',
-  anchor: 'left',
-}, ({ drawerOpened }: StyledDrawerProps)=> ({
-  ...(drawerOpened && {
-    width: 60,
-    '& .MuiDrawer-paper': { width: 60 },
-  }),
-  ...(!drawerOpened && {
-    width: 120,
-    '& .MuiDrawer-paper': 120,
-  }),
-}));
 export default function Header() {
+  const [opened, setOpen] = useState(true);
+  const handleDrawer = useCallback(() => {
+    setOpen(!opened);
+  }, [setOpen, opened]);
+
   const startItems = useMemo(() => (<AlignBox>
-    <MenuButton color="inherit" aria-label="menu">
+    <MenuButton color="inherit" aria-label="menu" onClick={handleDrawer}>
       <MenuIcon />
     </MenuButton>
     <LogoButton />
-  </AlignBox>), []);
+  </AlignBox>), [handleDrawer]);
 
   const centerItems = useMemo(() => (<AlignBox>
     <SearchInputBase placeholder="Search" />
@@ -101,11 +79,6 @@ export default function Header() {
   const endItems = useMemo(() => (<AlignBox>
     <AccountButton />
   </AlignBox>), []);
-  const [opened, setOpen] = useState(true);
-
-  const handleDrawer = () => {
-    setOpen(!opened);
-  };
 
   return (
     <Box>
@@ -114,47 +87,9 @@ export default function Header() {
         {startItems}
         {centerItems}
         {endItems}
-        <IconButton onClick={handleDrawer} size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-          <MenuIcon />
-        </IconButton>
-        <Box sx={{ flexGrow: 1 }}>
-          <Typography variant="h6" component="div">
-            Youtube
-          </Typography>
-        </Box>
-        <SearchBox>
-          <SearchInputBase placeholder="Search" />
-          <Button variant="outlined" startIcon={<SearchIcon />} />
-        </SearchBox>
-        <Button color="inherit" sx={{ padding: '0px' }}>
-          Login
-        </Button>
       </StyledToolbar>
     </StyledAppBar>
-    <StyledDrawer variant="permanent" drawerOpened={opened}>
-    <Toolbar />
-    <List>
-      {['홈', '탐색', '구독', 'originals'].map((text, index) => (
-        <ListItem button key={text}>
-          <ListItemIcon>
-            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-          </ListItemIcon>
-          <ListItemText primary={text} />
-        </ListItem>
-      ))}
-    </List>
-    <Divider />
-    <List>
-      {['홈', '탐색', '구독', 'originals'].map((text, index) => (
-        <ListItem button key={text}>
-          <ListItemIcon>
-            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-          </ListItemIcon>
-          <ListItemText primary={text} />
-        </ListItem>
-      ))}
-    </List>
-  </StyledDrawer>
+    <CustomDrawer opened={opened}/>
   </Box>
   );
 }
