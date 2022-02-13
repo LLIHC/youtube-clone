dbb044d0c77a4b644d6f0f954750f417bb1571f7
hunhoon21 <hunhoon21@gmail.com>
2022-02-13 15:57:45 +0900

    :sparkles: create a channel w/ POST api

--- backend/src/main.py
+++ backend/src/main.py
@@ -23,11 +23,17 @@ def get_db():
     finally:
         db.close()
 
+@app.post("/channels/", response_model=schemas.Channel)
+def create_user(channel: schemas.ChannelCreate, db: Session = Depends(get_db)):
+    # db_channel = crud.get_user_by_email(db, email=user.email)
+    # if db_channel:
+    #     raise HTTPException(status_code=400, detail="Channel already registered")
+    return crud.create_channel(db=db, channel=channel)
+
 @app.get("/channels/", response_model=List[schemas.Channel])
 def get_channels(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     channels = crud.get_channels(db, skip=skip, limit=limit)
     return channels
-
 
 # @app.get("/contents/", response_model)
 # def read_contents(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
