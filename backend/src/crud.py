18f31442f5ccc0e90e2193df5579751bec907dab
hunhoon21 <hunhoon21@gmail.com>
2022-02-13 15:20:52 +0900

    :hammer: add get channels method

--- backend/src/crud.py
+++ backend/src/crud.py
@@ -5,3 +5,6 @@ from . import models
 
 def get_contents(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Content).offset(skip).limit(limit).all()
+
+def get_channels(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Channel).offset(skip).limit(limit).all()
