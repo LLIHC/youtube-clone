928d2d01aa1437e03c9edec16f2a04f9995cc6c8
kynk94 <kynk94@naver.com>
2022-02-13 21:52:40 +0900

    add ThemeProvider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,22 @@
 import './index.css';
 import React from 'react';
 
+import { ThemeProvider } from '@mui/material/styles';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 import App from './App';
+import themes from './styles/themes';
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ThemeProvider theme={themes}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root'),
