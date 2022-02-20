013e8cefd24b4a7b346fe1bc165b7b5e1632ccc2
kynk94 <kynk94@naver.com>
2022-02-20 21:20:08 +0900

    rename isExpanded prop to expand

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -89,7 +89,7 @@ export default function Header() {
           {endItems}
         </StyledToolbar>
       </StyledAppBar>
-      <Drawer isExpanded={isDrawerExpanded} />
+      <Drawer expand={isDrawerExpanded} />
     </Box>
   );
 }
