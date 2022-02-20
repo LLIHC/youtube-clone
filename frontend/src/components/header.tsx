e05e2ceae4597c2cb2d00698a0b83cafaccf94e0
kynk94 <kynk94@naver.com>
2022-02-20 20:32:04 +0900

    resolve conflicts

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -6,10 +6,12 @@ import SearchIcon from '@mui/icons-material/Search';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Icon from '@mui/material/Icon';
 import IconButton from '@mui/material/IconButton';
 import InputBase from '@mui/material/InputBase';
 import Toolbar from '@mui/material/Toolbar';
 
+import { ReactComponent as Logo } from '../assets/logo-icon.svg';
 import AccountButton from './account/account';
 import Drawer from './drawer';
 import LogoButton from './logo-button';
@@ -20,8 +22,8 @@ const StyledAppBar = styled(AppBar)({
   display: 'block',
   height: '56px',
   width: '100%',
+  zIndex: 1400,
   top: 0,
-  zIndex: '1400',
 });
 
 const StyledToolbar = styled(Toolbar)({
@@ -45,12 +47,32 @@ const MenuButton = styled(IconButton)({
   padding: '8px',
 });
 
+const LogoButton = styled(IconButton)({
+  padding: 0,
+});
+
+const LogoIcon = styled(Icon)({
+  width: '90px',
+  height: '20px',
+  padding: '18px 14px 18px 16px',
+});
+
 const SearchButton = styled(Button)({
   padding: 0,
   height: '40px',
 });
 
-const AlignBox = styled(Box)({
+const Start = styled(Box)({
+  display: 'flex',
+  alignItems: 'center',
+});
+
+const Center = styled(Box)({
+  display: 'flex',
+  alignItems: 'center',
+});
+
+const End = styled(Box)({
   display: 'flex',
   alignItems: 'center',
 });
@@ -61,6 +83,33 @@ export default function Header() {
   const handleDrawer = useCallback(() => {
     setIsExpand(!isExpanded);
   }, [setIsExpand, isExpanded]);
+  
+  const navigate = useNavigate();
+  const handleLogoClick = useCallback(() => {
+    navigate('/');
+  }, [navigate]);
+
+  const startItems = useMemo(() => (<Start>
+    <MenuButton color="inherit" aria-label="menu" onClick={handleDrawer}>
+      <MenuIcon />
+    </MenuButton>
+    <LogoButton color="inherit" aria-label="logo" onClick={handleLogoClick}>
+      <LogoIcon>
+        <Logo />
+      </LogoIcon>
+    </LogoButton>
+  </Start>), [handleDrawer]);
+
+  const centerItems = useMemo(() => (<Center>
+    <SearchInputBase placeholder="Search" />
+    <SearchButton variant="outlined" aria-label="search">
+      <SearchIcon />
+    </SearchButton>
+  </Center>), []);
+
+  const endItems = useMemo(() => (<End>
+    <AccountButton />
+  </End>), []);
 
   const startItems = useMemo(() => (<AlignBox>
     <MenuButton color="inherit" aria-label="menu" onClick={handleDrawer}>
