5c600ebce2c80a24f71b560226a2827d13cf7e62
chad <ec_park@korea.ac.kr>
2022-02-20 18:43:42 +0900

    change: variable name & yarn lint

--- frontend/src/components/drawer.tsx
+++ frontend/src/components/drawer.tsx
@@ -16,18 +16,18 @@ const StyledDrawer = styled(MuiDrawer)({
   whiteSpace: 'nowrap',
   boxSizing: 'border-box',
   anchor: 'left',
-}, ({ expand }: StyledDrawerProps)=> ({
-  width: expand ? 60 : 200,
-  '& .MuiDrawer-paper': { width: expand ? 60 : 200 },
+}, ({ isExpanded }: StyledDrawerProps)=> ({
+  width: isExpanded ? 60 : 200,
+  '& .MuiDrawer-paper': { width: isExpanded ? 60 : 200 },
 }));
 
 interface StyledDrawerProps {
-  expand : boolean,
+  isExpanded : boolean,
 }
 
-export default function Drawer({ expand } : StyledDrawerProps) {
+export default function Drawer({ isExpanded } : StyledDrawerProps) {
   return (
-    <StyledDrawer variant="permanent" expand={expand}>
+    <StyledDrawer variant="permanent" isExpanded={isExpanded}>
     <Toolbar />
     <List>
       {['홈', '탐색', '구독', 'originals'].map((text, index) => (
@@ -41,7 +41,7 @@ export default function Drawer({ expand } : StyledDrawerProps) {
     </List>
     <Divider />
     <List>
-      {['홈', '탐색', '구독', 'originals'].map((text, index) => (
+      {['Home', 'Exploration', 'Subscriptions', 'whatever'].map((text, index) => (
         <ListItem button key={text}>
           <ListItemIcon>
             {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
