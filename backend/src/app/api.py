16f7f6f185651319e47a22a94dc8d4c7b09a3d01
hunhoon21 <hunhoon21@gmail.com>
2022-02-27 22:57:54 +0900

    :hammer: link API w/ router for contents

--- backend/src/app/api.py
+++ backend/src/app/api.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 
-from app.endpoints import channels
+from app.endpoints import channels, contents
 
 api_router = APIRouter()
 api_router.include_router(
     channels.router, prefix="/channels", tags=["channels"]
+)
+api_router.include_router(
+    contents.router, prefix="/contents", tags=["contents"]
 )
