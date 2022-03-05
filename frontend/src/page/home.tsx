ad5c9d0be475e378b503b4fa4c3d67a2dd9029cd
LEEMINJOO <eminj1997@gmail.com>
2022-03-06 01:14:47 +0900

    YC-29 클릭했을 때 새로운 비디로 로드

--- frontend/src/page/home.tsx
+++ frontend/src/page/home.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
 import styled from '@emotion/styled';
 import Grid from '@mui/material/Grid';
@@ -22,8 +22,67 @@ const StyledGrid = styled(Grid)({
   width: `calc(${nrow}/2 + ${imargin} - 0.01px)`,
 }));
 
+
+const MAX_VIDEO_IDS = 10;
+
+
+function RangeRemainder10(start: number, end: number) {
+  const full: number[] = Array.from(
+    Array(end - start + 1).keys(),
+  );
+  return full.map((_, idx) => (start + idx) % 10);
+}
+
+
+function fetchVideoIds(cursor: number, count: number = 4) {
+  const sampleVideoIds = useRecoilValue(sampleVideoIdsAtom);
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasMore, setHasMore] = useState(true);
+  const [nextCursor, setNextCursor] = useState(0);
+  const [videoIds, setVideoIds] = useState<string[]>([]);
+
+  const send = useCallback(
+    async () => {
+      await setIsLoading(true);
+
+      await setNextCursor(cursor + count);
+      const nextIndex = RangeRemainder10(cursor, cursor + count - 1);
+      const nextVideoIds = nextIndex.map(
+        index => sampleVideoIds[index],
+      );
+      setVideoIds(
+        (prev) => [...prev, ...nextVideoIds],
+      );
+
+      // mock fetching time
+      setTimeout(() => setIsLoading(false), 3000);
+    }, [cursor, count],
+  );
+
+  useEffect(
+    () => {
+      send();
+    }, [cursor, count],
+  );
+
+  useEffect(
+    () => {
+      setHasMore(nextCursor < MAX_VIDEO_IDS);
+    }, [setHasMore, nextCursor],
+  );
+
+  return { videoIds, isLoading, hasMore, nextCursor };
+}
+
+
 export default function Home() {
-  const videoIds = useRecoilValue(sampleVideoIdsAtom);
+  const [cursor, setCursor] = useState(1);
+  const { videoIds, isLoading, hasMore, nextCursor } = fetchVideoIds(cursor);
+
+  const handleClick = useCallback(() => {
+    setCursor(nextCursor);
+  }, [nextCursor]);
 
   const videoGrid = useMemo(() => {
     return videoIds.map((videoId, index) => (
@@ -34,8 +93,14 @@ export default function Home() {
   }, [videoIds]);
 
   return (
-    <Grid container spacing={2}>
-      {videoGrid}
-    </Grid>
+    <div>
+      <Grid container spacing={2}>
+        {videoGrid}
+      </Grid>
+      <button onClick={handleClick}>
+        버튼
+      </button>
+    </div>
+
   );
 }
