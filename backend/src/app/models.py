e8843de2d75f37d0f90b3fecc9affefa5db328be
HoonCheol Shin <36983960+hunhoon21@users.noreply.github.com>
2022-03-06 13:31:22 +0900

    ✨ Yc-24 DB Table 설계 (#14)
    * :heavy_plus_sign: add sqlalchemy
    
    * :heavy_plus_sign: add uvicorn
    
    * :sparkles: introduce database
    
    * :sparkles: create Channel & Content Model for mysql
    
    * :hammer: add new line
    
    * Update backend/requirements.txt
    
    * Update backend/src/models.py
    
    * Update backend/src/models.py
    
    * :hammer: add integer id
    
    * :hammer: change column name and set default as 0
    
    * :hammer: change column
    
    * :sparkles: add schemas for crud & api
    
    * :hammer: make it easy
    
    * :truck: move files to app directory
    
    * Update backend/src/app/schemas.py
    
    * :hammer: remove meanless variable name part
    
    * :hammer: make precommit happy :)

---
+++ backend/src/app/models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class Channel(Base):
+    __tablename__ = "channels"
+
+    id = Column(Integer, primary_key=True, index=True)
+    hashed_id = Column(String, unique=True)
+    thumbnail_url = Column(String, index=True)
+    name = Column(String)
+    # n_followers = Column(Integer, default=0)
+
+    contents = relationship("Content", back_populates="owner")
+
+
+class Content(Base):
+    __tablename__ = "contents"
+
+    id = Column(Integer, primary_key=True, index=True)
+    hashed_id = Column(String, unique=True)
+    title = Column(String)
+    # n_views = Column(Integer)
+    # upload_date = Column(DateTime)
+    description = Column(String)
+    # n_likes = Column(Integer)
+    thumbnail_url = Column(String)
+    channel_id = Column(Integer, ForeignKey("channels.hashed_id"))
+    owner = relationship("Channel", back_populates="contents")
